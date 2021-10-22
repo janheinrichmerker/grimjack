65aa58fdf3c00c43be963db6020cf07253ab6026
Jan Heinrich Reimer <heinrich@reimer.family>
2021-10-22 13:20:56 +0200

    Implement automatic indexing

--- grimjack/data.py
+++ grimjack/data.py
@@ -1,3 +1,5 @@
+from subprocess import run
+
 from dload import save_unzip
 
 from grimjack import PROJECT_DIR
@@ -7,6 +9,7 @@ TOPICS_ZIP_ULR = "https://webis.de/events/touche-22/data/topics-task2-2022.zip"
 
 DATA_DIR = PROJECT_DIR / "data"
 DOCUMENTS_DIR = DATA_DIR / "documents"
+INDEX_DIR = DATA_DIR / "index"
 TOPICS_DIR = DATA_DIR / "topics"
 
 
@@ -25,3 +28,23 @@ def download_topics():
     if TOPICS_DIR.exists():
         return
     save_unzip(TOPICS_ZIP_ULR, str(TOPICS_DIR), delete_after=True)
+
+
+def index_all():
+    index_documents()
+
+
+def index_documents():
+    if INDEX_DIR.exists():
+        return
+    run([
+        "python", "-m", "pyserini.index",
+        "-collection", "JsonCollection",
+        "-generator", "DefaultLuceneDocumentGenerator",
+        "-threads", "2",
+        "-input", str(DOCUMENTS_DIR.absolute()),
+        "-index", str(INDEX_DIR.absolute()),
+        "-storePositions",
+        "-storeDocvectors",
+        "-storeRaw"
+    ])
--- grimjack/run.py
+++ grimjack/run.py
@@ -1,9 +1,10 @@
-from grimjack.data import download_all
+from grimjack.data import download_all, index_all
 
 
 def main():
     print("Hello Grimjack!")
     download_all()
+    index_all()
 
 
 if __name__ == '__main__':
