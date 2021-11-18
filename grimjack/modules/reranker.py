8183d81d6db3e7b6c8150873afb96b6afd53c374
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-18 01:17:47 +0100

    Implement axiomatic reranker and kwiksort (with random pivot)

--- grimjack/modules/reranker.py
+++ grimjack/modules/reranker.py
@@ -1,18 +1,46 @@
-from pathlib import Path
-from typing import List, Optional, Set
+from dataclasses import dataclass
+from random import randint
+from typing import List
 
 from grimjack.model import RankedDocument
+from grimjack.model.axiom import Axiom
 from grimjack.modules import Reranker
 
 
+@dataclass
 class AxiomaticReranker(Reranker):
-    targer_api_url: str
-    models: Set[str]
-    cache_path: Optional[Path] = None
+    axiom: Axiom
+
+    def kwiksort(
+            self,
+            query: str,
+            vertices: list[RankedDocument]
+    ) -> list[RankedDocument]:
+        if len(vertices) == 0:
+            return []
+
+        vertices_left = []
+        vertices_right = []
+
+        # Select random pivot.
+        pivot = vertices[randint(0, len(vertices))]
+
+        for vertex in vertices:
+            if vertex == pivot:
+                continue
+
+            if self.axiom.preference(query, vertex, pivot) >= 0:
+                vertices_left.append(vertex)
+            else:
+                vertices_right.append(vertex)
+
+        return [*vertices_left, pivot, *vertices_right]
 
     def rerank(
             self,
             query: str,
             ranking: List[RankedDocument]
     ) -> List[RankedDocument]:
-        raise NotImplementedError()
+        ranking = ranking.copy()
+        ranking = self.kwiksort(query, ranking)
+        return ranking
