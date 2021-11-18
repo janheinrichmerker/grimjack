e99f09e42702f04a2a423b1daa57f3a3d767a0a9
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-18 01:25:08 +0100

    Add cached axiom (this should prevent computing the same preference again)

--- grimjack/model/axiom.py
+++ grimjack/model/axiom.py
@@ -39,3 +39,23 @@ class AggregatedAxiom(Axiom):
             axiom.preference(query, document1, document2)
             for axiom in self.axioms
         )
+
+
+class CachedAxiom(Axiom):
+    axiom: Axiom
+    _cache: dict[tuple[str, str], float]
+
+    def preference(
+            self,
+            query: str,
+            document1: RankedDocument,
+            document2: RankedDocument
+    ) -> float:
+        if (document1.id, document2.id) in self._cache:
+            return self._cache[document1.id, document2.id]
+        elif (document2.id, document1.id) in self._cache:
+            return -self._cache[document1.id, document2.id]
+        else:
+            preference = self.axiom.preference(query, document1, document2)
+            self._cache[document1.id, document2.id] = preference
+            return preference
