88e5dd6b368037a9da02b5f647e03f02e1ea8921
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-20 17:08:28 +0100

    Revert to simple model

--- grimjack/model/__init__.py
+++ grimjack/model/__init__.py
@@ -1,82 +1,23 @@
-from abc import abstractmethod, ABC
+from dataclasses import dataclass
 from typing import Dict
 
 
-class Text(ABC):
-    @property
-    @abstractmethod
-    def text(self) -> str:
-        pass
-
-    @property
-    @abstractmethod
-    def terms(self) -> list[str]:
-        pass
-
-    @property
-    @abstractmethod
-    def term_set(self) -> set[str]:
-        pass
-
-    @abstractmethod
-    def term_frequency(self, term: str) -> float:
-        pass
+@dataclass
+class Query:
+    id: int
+    title: str
+    description: str
+    narrative: str
 
 
-class Query(Text):
-    @property
-    def text(self) -> str:
-        return self.title
-
-    @property
-    @abstractmethod
-    def id(self) -> int:
-        pass
-
-    @property
-    @abstractmethod
-    def title(self) -> str:
-        pass
-
-    @property
-    @abstractmethod
-    def description(self) -> str:
-        pass
-
-    @property
-    @abstractmethod
-    def narrative(self) -> str:
-        pass
+@dataclass
+class Document:
+    id: str
+    content: str
+    fields: Dict[str, str]
 
 
-class Document(Text):
-    @property
-    def text(self) -> str:
-        return self.content
-
-    @property
-    @abstractmethod
-    def id(self) -> str:
-        pass
-
-    @property
-    @abstractmethod
-    def content(self) -> str:
-        pass
-
-    @property
-    @abstractmethod
-    def fields(self) -> Dict[str, str]:
-        pass
-
-
+@dataclass
 class RankedDocument(Document):
-    @property
-    @abstractmethod
-    def score(self) -> float:
-        pass
-
-    @property
-    @abstractmethod
-    def rank(self) -> int:
-        pass
+    score: float
+    rank: int
