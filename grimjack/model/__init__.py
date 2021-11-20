57bfb60f93bb590e888e212669a604fc938f7c69
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-20 16:07:51 +0100

    Add text model

--- grimjack/model/__init__.py
+++ grimjack/model/__init__.py
@@ -1,5 +1,27 @@
+from abc import abstractmethod, ABC
 from dataclasses import dataclass
 from typing import Dict
+
+
+class Text(ABC):
+    @property
+    @abstractmethod
+    def text(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def terms(self) -> list[str]:
+        pass
+
+    @property
+    @abstractmethod
+    def term_set(self) -> set[str]:
+        pass
+
+    @abstractmethod
+    def term_frequency(self, term: str) -> float:
+        pass
 
 
 @dataclass
