c16ac964f72e778e2f54b398bcede190a6956785
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-22 01:21:54 +0100

    Cache functions

--- grimjack/modules/reranking_context.py
+++ grimjack/modules/reranking_context.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from functools import cached_property
+from functools import cached_property, cache
 from math import log
 from typing import List, Set
 
@@ -13,7 +13,7 @@ class IndexRerankingContext(RerankingContext):
     index: Index
 
     @cached_property
-    def _index_reader(self):
+    def _index_reader(self) -> IndexReader:
         return IndexReader(str(self.index.index_dir.absolute()))
 
     @cached_property
@@ -29,12 +29,14 @@ class IndexRerankingContext(RerankingContext):
             return 0
         return log(self.document_count / document_frequency)
 
+    @cache
     def terms(self, text: str) -> List[str]:
         return self._index_reader.analyze(text)
 
     def term_set(self, text: str) -> Set[str]:
         return set(self.terms(text))
 
+    @cache
     def term_frequency(self, text: str, term: str) -> float:
         # TODO: Is this correctly implemented?
         terms = self.terms(text)
