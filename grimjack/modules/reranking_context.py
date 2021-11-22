e43e4cb27b31a20f76211752f06bc5bd20f99798
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-22 01:23:41 +0100

    Add retrieval scores to reranking context

--- grimjack/modules/reranking_context.py
+++ grimjack/modules/reranking_context.py
@@ -5,7 +5,11 @@ from typing import List, Set
 
 from pyserini.index import IndexReader
 
+from grimjack.model import Query, Document
 from grimjack.modules import Index, RerankingContext
+from grimjack.utils.jvm import JBM25Similarity, JDFRSimilarity, JBasicModelIn, \
+    JAfterEffectL, JNormalizationH2, JSimilarity, JLMDirichletSimilarity, \
+    JTFIDFSimilarity
 
 
 @dataclass(unsafe_hash=True)
@@ -42,3 +46,74 @@ class IndexRerankingContext(RerankingContext):
         terms = self.terms(text)
         term_count = sum(1 for other in terms if other == term)
         return term_count / len(terms)
+
+    @staticmethod
+    @cache
+    def _tf_idf_similarity() -> JSimilarity:
+        return JTFIDFSimilarity()
+
+    def tf_idf_score(
+            self,
+            query: Query,
+            document: Document
+    ) -> float:
+        return self._index_reader.compute_query_document_score(
+            document.id,
+            query.title,
+            self._tf_idf_similarity()
+        )
+
+    @staticmethod
+    @cache
+    def _bm25_similarity(k1: float = 1.2, b: float = 0.75) -> JSimilarity:
+        return JBM25Similarity(k1, b)
+
+    def bm25_score(
+            self,
+            query: Query,
+            document: Document,
+            k1: float = 1.2,
+            b: float = 0.75
+    ) -> float:
+        return self._index_reader.compute_query_document_score(
+            document.id,
+            query.title,
+            self._bm25_similarity(k1, b)
+        )
+
+    @staticmethod
+    @cache
+    def _pl2_similarity(c: float = 0.1) -> JSimilarity:
+        return JDFRSimilarity(
+            JBasicModelIn(),
+            JAfterEffectL(),
+            JNormalizationH2(c)
+        )
+
+    def pl2_score(
+            self,
+            query: Query,
+            document: Document,
+            c: float = 0.1
+    ) -> float:
+        return self._index_reader.compute_query_document_score(
+            document.id,
+            query.title,
+            self._pl2_similarity(c)
+        )
+
+    @staticmethod
+    def _ql_similarity(mu: float = 1000) -> JSimilarity:
+        return JLMDirichletSimilarity(mu)
+
+    def ql_score(
+            self,
+            query: Query,
+            document: Document,
+            mu: float = 1000
+    ) -> float:
+        return self._index_reader.compute_query_document_score(
+            document.id,
+            query.title,
+            self._ql_similarity(mu)
+        )
