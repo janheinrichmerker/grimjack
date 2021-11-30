a91b7d5727f1d883217d1db51c4b1b2eb6856300
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-30 11:08:49 +0100

    Implement TARGER tagger module

--- grimjack/modules/argument_tagger.py
+++ grimjack/modules/argument_tagger.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional, Set
 
+from grimjack.api.targer import fetch_arguments
 from grimjack.model import RankedDocument
 from grimjack.model.arguments import ArgumentRankedDocument
 
@@ -8,8 +11,17 @@ from grimjack.modules import ArgumentTagger
 
 @dataclass
 class TargerArgumentTagger(ArgumentTagger):
+    targer_api_url: str
+    models: Set[str]
+    cache_path: Optional[Path] = None
+
     def tag_arguments(
             self,
             document: RankedDocument
     ) -> ArgumentRankedDocument:
-        raise NotImplementedError()
+        return fetch_arguments(
+            self.targer_api_url,
+            self.models,
+            document,
+            self.cache_path
+        )
