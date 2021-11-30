ec7fef11bcffa6a29236f32a6e42614349149c9b
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-30 13:22:15 +0100

    Implement IBM Debater caching

--- grimjack/model/quality.py
+++ grimjack/model/quality.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List
+
+from dataclasses_json import dataclass_json
 
 from grimjack.model.arguments import ArgumentRankedDocument
 
 
+@dataclass_json
 @dataclass
 class ArgumentQualitySentence:
     content: str
--- grimjack/modules/argument_quality_tagger.py
+++ grimjack/modules/argument_quality_tagger.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
 
-from grimjack.api.debater import get_quality_score
+from grimjack.api.debater import get_quality_scores
 from grimjack.model import Query
 from grimjack.model.arguments import ArgumentRankedDocument
 from grimjack.model.quality import ArgumentQualityRankedDocument
@@ -10,14 +12,26 @@ from grimjack.modules import ArgumentQualityTagger
 @dataclass
 class DebaterArgumentQualityTagger(ArgumentQualityTagger):
     debater_api_token: str
+    cache_path: Optional[Path] = None
 
     def tag_document(
             self,
             query: Query,
             document: ArgumentRankedDocument
     ) -> ArgumentQualityRankedDocument:
-        return get_quality_score(
+        quality = get_quality_scores(
             query,
             document,
-            self.debater_api_token
+            self.debater_api_token,
+            self.cache_path
+        )
+
+        return ArgumentQualityRankedDocument(
+            id=document.id,
+            content=document.content,
+            fields=document.fields,
+            score=document.score,
+            rank=document.rank,
+            arguments=document.arguments,
+            quality=quality
         )
