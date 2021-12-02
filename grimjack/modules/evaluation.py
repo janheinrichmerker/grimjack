2179515fe7598a09dc712d04dfdc297f7b95742c
Jan Heinrich Reimer <heinrich@reimer.family>
2021-12-02 14:11:10 +0100

    Add passage to document run file merging

--- grimjack/modules/evaluation.py
+++ grimjack/modules/evaluation.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
+from logging import warning, info
 from pathlib import Path
 from typing import Dict
 
-from pandas import DataFrame
+from pandas import DataFrame, NamedAgg
 from trectools import TrecRun, TrecEval
 
 from grimjack.modules import Evaluation, QrelsStore
@@ -16,7 +17,50 @@ class TrecEvaluation(Evaluation):
 
     def _evaluation(self, run_file: Path):
         run = TrecRun(run_file)
-        return TrecEval(run, self.qrels_store.qrels)
+        qrels = self.qrels_store.qrels
+
+        run_docs = set(run.run_data["docid"])
+        qrels_docs = set(qrels.qrels_data["docid"])
+
+        if len(run_docs & qrels_docs) == 0:
+            warning(
+                "The chosen qrels contain no relevance judgements "
+                "for documents from the run file.\n"
+                "You're likely using document-level qrels "
+                "but a passage-level run file."
+            )
+            info(
+                "Trying to merge run file passages into documents "
+                "to evaluate on the document level.\n"
+                "The top passage's score and rank is used "
+                "as the document's score and rank."
+            )
+            run_data: DataFrame = run.run_data.copy()
+            run_data["docid"] = run_data["docid"].apply(
+                lambda id: id.split("___")[0]
+            )
+            run_data = run_data.groupby([
+                "query",
+                "q0",
+                "docid",
+                "system"
+            ]).aggregate(
+                rank=NamedAgg(column="rank", aggfunc="min"),
+                score=NamedAgg(column="score", aggfunc="max"),
+            ).reset_index()
+            run_data = run_data.sort_values(by=["score"], ascending=False)
+            run_data = run_data.sort_values(
+                by=[
+                    "q0",
+                    "system",
+                    "query",
+                    "score"
+                ],
+                ascending=False
+            )
+            run.run_data = run_data
+
+        return TrecEval(run, qrels)
 
     def evaluate(self, run_file: Path, depth: int) -> float:
         evaluation = self._evaluation(run_file)
