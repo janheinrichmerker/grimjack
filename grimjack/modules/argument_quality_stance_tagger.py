a983fecfe541560039c2365588ff48e7bffc09a2
Jan Heinrich Reimer <heinrich@reimer.family>
2021-12-16 16:19:00 +0100

    Add threshold stance tagger

--- grimjack/modules/argument_quality_stance_tagger.py
+++ grimjack/modules/argument_quality_stance_tagger.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from typing import Optional, List
 from grimjack.model import Query
 from grimjack.model.quality import ArgumentQualityRankedDocument
-from grimjack.model.stance import ArgumentQualityStanceRankedDocument
+from grimjack.model.stance import (
+    ArgumentQualityStanceRankedDocument, ArgumentStanceSentence
+)
 from grimjack.api.debater import get_stance_scores, preload_stance_scores
 from grimjack.modules.options import StanceCalculation
 
@@ -54,4 +56,58 @@ class DebaterArgumentQualityStanceTagger(ArgumentQualityStanceTagger):
             arguments=document.arguments,
             qualities=document.qualities,
             stances=stances
+        )
+
+
+@dataclass
+class ThresholdArgumentQualityStanceTagger(ArgumentQualityStanceTagger):
+    tagger: ArgumentQualityStanceTagger
+    threshold: float = 0.5
+
+    def _sentence_threshold(
+            self,
+            sentence: ArgumentStanceSentence
+    ) -> ArgumentStanceSentence:
+        return ArgumentStanceSentence(
+            content=sentence.content,
+            stance=(
+                sentence.stance
+                if abs(sentence.stance) >= self.threshold
+                else 0
+            ),
+        )
+
+    def _document_threshold(
+            self,
+            document: ArgumentQualityStanceRankedDocument
+    ) -> ArgumentQualityStanceRankedDocument:
+        return ArgumentQualityStanceRankedDocument(
+
+            id=document.id,
+            content=document.content,
+            fields=document.fields,
+            score=document.score,
+            rank=document.rank,
+            arguments=document.arguments,
+            qualities=document.qualities,
+            stances=[
+                self._sentence_threshold(sentence)
+                for sentence in document.stances
+            ],
+        )
+
+    def tag_ranking(
+            self,
+            query: Query,
+            ranking: List[ArgumentQualityRankedDocument]
+    ) -> List[ArgumentQualityStanceRankedDocument]:
+        ranking = self.tagger.tag_ranking(query, ranking)
+        return [self._document_threshold(document) for document in ranking]
+
+    def tag_document(
+            self, query: Query,
+            document: ArgumentQualityRankedDocument
+    ) -> ArgumentQualityStanceRankedDocument:
+        return self._document_threshold(
+            self.tagger.tag_document(query, document)
         )
