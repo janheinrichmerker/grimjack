832b566b635d79bb61a0633a8806c09393911ad0
Jan Heinrich Reimer <heinrich@reimer.family>
2021-12-18 12:38:00 +0100

    Add Huggingface stance tagger

--- grimjack/modules/argument_quality_stance_tagger.py
+++ grimjack/modules/argument_quality_stance_tagger.py
@@ -8,6 +8,7 @@ from typing import Optional, List
 from nltk import sent_tokenize
 
 from grimjack.api.debater import CachedDebaterArgumentStanceScorer
+from grimjack.api.huggingface import CachedHuggingfaceTextGenerator
 from grimjack.model import Query
 from grimjack.model.quality import ArgumentQualityRankedDocument
 from grimjack.model.stance import (
@@ -140,6 +141,125 @@ class DebaterArgumentQualitySentimentStanceTagger(
             f"{comparative_object} is good",
             f"{comparative_object} is the best"
         ]
+
+
+@dataclass
+class HuggingfaceArgumentQualityStanceTagger(ArgumentQualityStanceTagger):
+    model: str
+    api_key: str
+    cache_path: Optional[Path] = None
+
+    @contextmanager
+    def _generator(self) -> CachedHuggingfaceTextGenerator:
+        with CachedHuggingfaceTextGenerator(
+                model=self.model,
+                api_key=self.api_key,
+                cache_dir=self.cache_path,
+        ) as generator:
+            yield generator
+
+    @staticmethod
+    def _task(comparative_object: str, sentence: str) -> str:
+        return (
+            f"Is this sentence positive or negative "
+            f"with respect to \"{comparative_object}\"? {sentence}"
+        )
+
+    def _stance_single_target(
+            self,
+            generator: CachedHuggingfaceTextGenerator,
+            comparative_object: str,
+            sentence: str
+    ) -> float:
+        task = self._task(comparative_object, sentence)
+        answer = generator.generate(task).strip().lower()
+        print(answer)
+        if answer == "positive":
+            return 1
+        elif answer == "negative":
+            return -1
+        else:
+            return 0
+
+    def _stance_multi_target(
+            self,
+            generator: CachedHuggingfaceTextGenerator,
+            query: Query,
+            sentence: str
+    ) -> float:
+        object_a, object_b = query.comparative_objects
+        stance_a = self._stance_single_target(generator, object_a, sentence)
+        stance_b = self._stance_single_target(generator, object_b, sentence)
+        return stance_a - stance_b
+
+    def _tag_document(
+            self,
+            generator: CachedHuggingfaceTextGenerator,
+            query: Query,
+            document: ArgumentQualityRankedDocument
+    ) -> ArgumentQualityStanceRankedDocument:
+        sentences = sent_tokenize(document.content)
+
+        stances: List[ArgumentStanceSentence]
+        if query.comparative_objects is None:
+            stances = [
+                ArgumentStanceSentence(sentence, 0)
+                for sentence in sentences
+            ]
+        else:
+            stances = [
+                ArgumentStanceSentence(
+                    sentence,
+                    self._stance_multi_target(
+                        generator,
+                        query,
+                        sentence
+                    )
+                )
+                for sentence in sentences
+            ]
+
+        return ArgumentQualityStanceRankedDocument(
+            id=document.id,
+            content=document.content,
+            fields=document.fields,
+            score=document.score,
+            rank=document.rank,
+            arguments=document.arguments,
+            qualities=document.qualities,
+            stances=stances
+        )
+
+    def tag_ranking(
+            self,
+            query: Query,
+            ranking: List[ArgumentQualityRankedDocument]
+    ) -> List[ArgumentQualityStanceRankedDocument]:
+        tasks = []
+        if query.comparative_objects is not None:
+            download_nltk_dependencies("punkt")
+            object_a, object_b = query.comparative_objects
+            tasks = [
+                self._task(comparative_object, sentence)
+                for comparative_object in (object_a, object_b)
+                for document in ranking
+                for sentence in sent_tokenize(document.content)
+            ]
+        with self._generator() as generator:
+            generator.preload(tasks)
+            return [
+                self._tag_document(generator, query, document)
+                for document in ranking
+            ]
+
+    def tag_document(
+            self,
+            query: Query,
+            document: ArgumentQualityRankedDocument
+    ) -> ArgumentQualityStanceRankedDocument:
+        download_nltk_dependencies("punkt")
+        with self._generator() as generator:
+            return self._tag_document(generator, query, document)
 
 
 @dataclass
