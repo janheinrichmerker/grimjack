e1e323d8e33fbf05e1345817f06cb0c1b0e2803e
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-27 20:14:09 +0100

    Add TIRA run script

---
+++ tira-run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Configure task inputs.
+topic_file_name="topics.xml"
+documents_file_name="passages.jsonl.gz"
+run_file_name="run.txt"
+
+# Parse script options (paths and tag).
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 input_dir output_dir run_tag [options...]"
+    exit 1
+fi
+input_dir=$1
+output_dir=$2
+run_tag=$3
+shift 3
+
+# Determine input and output file paths.
+input_topic_file="$input_dir/$topic_file_name"
+input_documents_file="$input_dir/$documents_file_name"
+output_run_file="$output_dir/$run_file_name"
+
+# Install project dependencies.
+#pipenv install
+
+# Generate internal run file.
+pipenv run python -m grimjack \
+  --topics-path "$input_topic_file" \
+  --documents-path "$input_documents_file" \
+  "$@" \
+  run \
+  --tag "$run_tag" \
+  "$output_run_file"
