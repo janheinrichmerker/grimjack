aa9f067b112e63ddb670647053932cbce9c147c4
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-27 16:42:59 +0100

    Fix code format

--- grimjack/model/test_model.py
+++ grimjack/model/test_model.py
@@ -13,18 +13,18 @@ def query() -> Query:
         title="Which is better, a laptop or a desktop?",
         comparative_objects=("laptop", "desktop"),
         description=dedent("""\
-        A user wants to buy a new PC but has no prior preferences. They want 
-        to find arguments that show in what personal situation what kind of 
-        machine is preferable. This can range from situations like frequent 
-        traveling where a mobile device is to be favored to situations of a 
+        A user wants to buy a new PC but has no prior preferences. They want \
+        to find arguments that show in what personal situation what kind of \
+        machine is preferable. This can range from situations like frequent \
+        traveling where a mobile device is to be favored to situations of a \
         rather "stationary" gaming desktop PC.
         """),
         narrative=dedent("""\
-        Highly relevant documents will describe the major similarities and 
-        dissimilarities of laptops and desktops along with the respective 
-        advantages and disadvantages of specific usage scenarios. A comparison 
-        of the technical and architectural characteristics without personal 
-        opinion, recommendation, or pros/cons is not relevant.
+        Highly relevant documents will describe the major similarities and \
+        dissimilarities of laptops and desktops along with the respective \
+        advantages and disadvantages of specific usage scenarios. A \
+        comparison of the technical and architectural characteristics without \
+        personal opinion, recommendation, or pros/cons is not relevant.
         """),
     )
 
