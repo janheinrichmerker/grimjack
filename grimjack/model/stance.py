7d1a0d59a61883cc5af4deeb92d4661240b29645
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-27 17:18:09 +0100

    Add stance label model

--- grimjack/model/stance.py
+++ grimjack/model/stance.py
@@ -1,10 +1,19 @@
 from dataclasses import dataclass
+from enum import Enum
+from functools import cached_property
 from statistics import mean
 from typing import List
 
 from dataclasses_json import DataClassJsonMixin
 
 from grimjack.model.quality import ArgumentQualityRankedDocument
+
+
+class Stance(Enum):
+    FIRST = "FIRST"  # Pro 1st object
+    SECOND = "SECOND"  # Pro 2nd object
+    NEUTRAL = "NEUTRAL"  # Neutral stance
+    NO = "NO"  # No stance
 
 
 @dataclass
@@ -17,6 +26,15 @@ class ArgumentStanceSentence(DataClassJsonMixin):
 class ArgumentQualityStanceRankedDocument(ArgumentQualityRankedDocument):
     stances: List[ArgumentStanceSentence]
 
-    @property
+    @cached_property
     def average_stance(self) -> float:
         return mean(sentence.stance for sentence in self.stances)
+
+    @cached_property
+    def average_stance_label(self) -> Stance:
+        if self.average_stance > 0:
+            return Stance.FIRST
+        elif self.average_stance < 0:
+            return Stance.SECOND
+        else:
+            return Stance.NEUTRAL
