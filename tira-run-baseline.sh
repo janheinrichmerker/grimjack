e13ecbf514516c6291545f0d8246b52b075852b3
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-27 20:20:01 +0100

    Add TIRA baseline run

---
+++ tira-run-baseline.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+./tira-run.sh "$1" "$2" "grimjack-baseline" \
+  --retrieval-model query-likelihood-dirichlet \
+  --targer-model tag-ibm-fasttext \
+  --stance-tagger sentiment \
+  --stance-threshold 0.5 \
+  --num-hits 100
