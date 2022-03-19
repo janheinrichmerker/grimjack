e76b70007e708a5789c8eae550f1e940fd8d47df
Jan Heinrich Reimer <heinrich@reimer.family>
2022-03-19 11:02:11 +0100

    Update TIRA setup

--- tira-setup.sh
+++ tira-setup.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ $UID != 0 ]]; then
-  echo "Please run this script with sudo:"
-  echo "sudo $0 $*"
-  exit 1
-fi
-
 # Install System dependencies.
-apt-get -y update && apt-get -y install \
+sudo apt-get -y update && apt-get -y install \
   git \
   openjdk-11-jdk \
   python3.9 \
