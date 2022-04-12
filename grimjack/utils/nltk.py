8bdabf04a0fd36aed62b8fba104b595be5277557
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-04-12 15:27:16 +0200

    Don't invalidate NLTK index

--- grimjack/utils/nltk.py
+++ grimjack/utils/nltk.py
@@ -5,6 +5,8 @@ from grimjack import logger
 
 def download_nltk_dependencies(*dependencies: str):
     downloader = Downloader()
+    # Don't invalidate NLTK index once installed.
+    downloader.INDEX_TIMEOUT = 1000 * 365 * 24 * 60 * 60  # 1000 years.
     for dependency in dependencies:
         if not downloader.is_installed(dependency):
             logger.info(f"Downloading NLTK dependency {dependency}.")
