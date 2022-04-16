0991b08cabde96c965398cbfb8ba6ac4464d26c9
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-04-16 23:58:08 +0200

    Skip NLTK download without network connection

--- grimjack/utils/nltk.py
+++ grimjack/utils/nltk.py
@@ -1,12 +1,21 @@
 from nltk.downloader import Downloader
+from requests import head
 
 from grimjack import logger
 
 
 def download_nltk_dependencies(*dependencies: str):
     downloader = Downloader()
-    # Don't invalidate NLTK index once installed.
-    downloader.INDEX_TIMEOUT = 1000 * 365 * 24 * 60 * 60  # 1000 years.
+
+    try:
+        head(Downloader.DEFAULT_URL, timeout=1)
+    except ConnectionError:
+        logger.warning(
+            "Could not connect to NLTK servers. "
+            "Skipping NLTK download."
+        )
+        return
+
     for dependency in dependencies:
         if not downloader.is_installed(dependency):
             logger.info(f"Downloading NLTK dependency {dependency}.")
